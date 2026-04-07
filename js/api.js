d50f46a0937310ad16b3b0f4674ffc2799951897
SalomAo-Goetzinger <salomaogoetzinger3@gmail.com>
2026-04-07 18:26:10 -0300

    Aula2 finalizada

--- js/api.js
+++ js/api.js
@@ -8,6 +8,22 @@ const api = {
       throw error;
     }
   },
+
+  async buscarPensamento(pensamento) {
+    try {
+      const response = await fetch("http://localhost:3000/pensamentos", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(pensamento),
+      });
+      return await response.json();
+    } catch {
+      alert("Ops, algo deu errado. Tente novamente mais tarde.");
+      throw error;
+    }
+  },
 };
 
 export default api;
